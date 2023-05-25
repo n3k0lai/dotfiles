39cf4bc8b900bdb8b8c515d2358ebcb7ae2f5a1e
nicholai <7215827+n3k0lai@users.noreply.github.com>
2023-05-24 20:20:51 -0400

    remove betterlockscreen, add swaylock-effects

--- .config/fish/functions/werk.fish
+++ .config/fish/functions/werk.fish
@@ -3,5 +3,5 @@ function werk
   source ~/.ssh/workcreds.fish
   or echo "creds not found, choom." # use set -x, no function wrapper
   and echo "logging into $WORK_IP"
-  xfreerdp /w:1920 /h:1040 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP
+  xfreerdp /w:1920 /h:1050 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP
 end
