18f0d709d7219ce7a543bbf97ac31bd9d2bf8a2c
nicholai <nicholai@comfy.sh>
2025-12-11 12:55:24 -0500

    update werk

--- .config/fish/functions/werk.fish
+++ .config/fish/functions/werk.fish
@@ -1,12 +1,29 @@
 # a windows RDP wrapper
 function werk --argument-names cmd
-    source ~/.ssh/workcreds.fish or echo "creds not found, choom." # use set -x, no function wrapper
-    and echo "logging into $WORK_IP"
+    # Force universal vars to be inherited even in non-login shells
+    if not set -q WORK_IP
+        source ~/.ssh/workcreds.fish 2>/dev/null
+        or begin
+            echo "creds not found, choom."
+            return 1
+        end
+    end
+
+    echo "logging into $WORK_IP"
+    
+    set -l clip_opts \
+        /clipboard \
+        +clipboard \
+        # /dynamic-resolution \
+        # /bpp:32 \
+        /cert:ignore \
+        +home-drive
+    
     switch "$cmd"
         # case for local ip
         case -s
-            sdl-freerdp /w:1920 /h:1080 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP /clipboard /cert:ignore
-        case ""
-            sdl-freerdp /w:2560 /h:1440 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP /clipboard /cert:ignore
+            sdl-freerdp /w:1920 /h:1080 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP $clip_opts
+        case "*"
+            sdl-freerdp /w:2560 /h:1440 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP $clip_opts
     end
 end
