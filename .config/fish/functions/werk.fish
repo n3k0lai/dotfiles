6a0bbbe1e4da4839c25806af3981ef01a9d4ed95
nicholai <nicholai@comfy.sh>
2025-12-11 12:20:34 -0500

    gitignore

--- .config/fish/functions/werk.fish
+++ .config/fish/functions/werk.fish
@@ -4,9 +4,9 @@ function werk --argument-names cmd
     and echo "logging into $WORK_IP"
     switch "$cmd"
         # case for local ip
-        case -l
-            sdl-freerdp /w:2560 /h:1340 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP /cert:ignore
+        case -s
+            sdl-freerdp /w:1920 /h:1080 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP /clipboard /cert:ignore
         case ""
-            sdl-freerdp /w:1920 /h:1050 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP /cert:ignore
+            sdl-freerdp /w:2560 /h:1440 /u:$WORK_USR /p:$WORK_PWD /v:$WORK_IP /clipboard /cert:ignore
     end
 end
