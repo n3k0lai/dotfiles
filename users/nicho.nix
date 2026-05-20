4dd539be94fb53e4c38ea104df6cd2797c9f68b5
ene <ene@comfy.sh>
2026-05-19 20:43:34 -0400

    Fix users/nicho.nix module structure

--- users/nicho.nix
+++ users/nicho.nix
@@ -1,11 +1,10 @@
 # users/nicho.nix
 # User-specific configuration for nicho
-{ config, lib, pkgs, ... }:
-
+{ config, lib, pkgs, ... }: {
   imports = [ ../modules/editors/grokbuild.nix ];
+
   modules.editors.grokbuild.enable = true;
 
-{
   home-manager.users.nicho = { pkgs, ... }: {
     # User environment variables
     home.sessionVariables = {
@@ -14,26 +13,26 @@
       TERMINAL = "kitty";
       BROWSER = "firefox";
       BROWSER_MIN = "luakit";
-      
+
       # XDG directories
       XDG_CONFIG_HOME = "$HOME/.config";
       XDG_DATA_HOME = "$HOME/.local/share";
-      
+
       # Development paths
       GOPATH = "$HOME/.local/share/go";
-      
+
       # Emacs/Doom
       DOOMDIR = "$HOME/.config/doom";
       EMACSDIR = "$HOME/.config/emacs";
-      
+
       # Application fixes
       MOZ_USE_XINPUT2 = "1";  # Mozilla smooth scrolling/touchpads
-      OBS_USE_EGL = "1";  # OBS game capture on X11
-      
+      OBS_USE_EGL = "1";      # OBS game capture on X11
+
       # Display (if not set by display manager)
       DISPLAY = ":0";
     };
-    
+
     # Additional PATH entries
     home.sessionPath = [
       "$HOME/.local/bin"
@@ -42,7 +41,7 @@
       "$HOME/.npm-global/bin"
       "$HOME/.dotnet/tools"
     ];
-    
+
     # Fish-specific interactive shell init
     programs.fish = {
       interactiveShellInit = ''
@@ -52,7 +51,7 @@
         end
       '';
     };
-    
+
     home.packages = with pkgs; [
       zip
       unzip
